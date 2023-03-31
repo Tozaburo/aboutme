1c7828c4e3e4cd959766597a6aaf981c495d03e0
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-03-31 17:28:03 +0900

    Create activity.js

---
+++ activity.js
@@ -0,0 +1,7 @@
+function replaceDes(result){
+  document.querySelector('.description').innerHTML = result;
+}
+
+function replaceWeek(result){
+  document.querySelector('.week').innerHTML = result;
+}
