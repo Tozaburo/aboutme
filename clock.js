c4f54f8fbf807cc95f40201264509592e15473a1
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-04-04 16:03:19 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -5,10 +5,9 @@ setInterval(() => {
 }, 100);
 
 function timeChanged() {
-  if (tsec != new Date().getSeconds()) {
+  if (tsec != new Date().getMilliseconds()) {
     replacetime();
-    tsec = new Date().getSeconds();
-    console.log(tsec);
+    tsec = new Date().getMilliseconds();
   }
 }
 
@@ -18,9 +17,12 @@ function replacetime() {
   var hour = now.getHours();
   var min = now.getMinutes();
   var sec = now.getSeconds();
+  var millisec = now.getMilliseconds();
   var shour = String(hour);
   var smin = String(min);
   var ssec = String(sec);
+
+  console.log(millisec);
 
   if (shour.length == 1) {
     shour = "0" + shour;
@@ -38,30 +40,53 @@ function replacetime() {
 
   document.querySelector(".time").innerHTML = result;
 
+  var csec = ssec;
+
   var cmin = min + sec / 60;
 
   var chour = hour + cmin / 60;
+
+  var gsec = "hsl(" + String(csec * 6) + " 80% 40%)";
 
   var gmin = "hsl(" + String(cmin * 6) + " 80% 40%)";
 
   var ghour = "hsl(" + String(chour * 15) + " 80% 40%)";
 
-  document.getElementById("main").style.background =
-    "linear-gradient(" + gmin + ", " + ghour + ")";
+  var backgroundsec = "linear-gradient(to right, transparent, " + gsec + ")";
+
+  var backgroundhm = "linear-gradient(" + gmin + ", " + ghour + ")";
+
+  var cssg = backgroundsec + "\n" + backgroundhm;
+  document.getElementById("main").style.background = backgroundhm;
+
+  if (show == 1) {
+    document.getElementById("overlay").style.background = backgroundsec;
+  } else {
+    document.getElementById("overlay").style.background = "transparent";
+  }
 }
 
 document.addEventListener("keydown", keydownEvent, false);
 
 var fontW = 200;
 
+var show = 0;
+
 function keydownEvent(event) {
   if (event.code === "KeyB") {
     if (fontW == 200) {
       fontW = 700;
-      console.log("700");
     } else {
-      console.log("200");
       fontW = 200;
+    }
+    document.getElementById("time").style.fontWeight = fontW;
+  }
+
+  if (event.code === "KeyS") {
+    if (show == 1) {
+      show = 0;
+    } else {
+      show = 1;
     }
     document.getElementById("time").style.fontWeight = fontW;
   }
