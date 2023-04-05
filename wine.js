16a812700abd2e23ae0d2cc71fb8fa1956049755
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-04-05 20:57:39 +0900

    Create wine.js

---
+++ wine.js
@@ -0,0 +1,11 @@
+function calc() {
+  var lrain = Number(document.getElementById("lrain").value);
+  var train = Number(document.getElementById("train").value);
+  var temp = Number(document.getElementById("temp").value);
+  var year = Number(document.getElementById("year").value);
+  var price =
+    lrain * 0.00117 - train * 0.00386 + temp * 0.616 + year * 0.02358 - 12.145;
+  console.log(price);
+  document.querySelector(".result").innerHTML =
+    "ワインの品質：" + String(price);
+}
