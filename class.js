e6a96a917b74d75387fb92c16a9f7e1c8cd8c9ea
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-04-19 18:33:01 +0900

    Update class.js

--- class.js
+++ class.js
@@ -1,23 +1,40 @@
-var now = new Date();
+var tmin = 61;
 
-// var hour = now.getHours();
-// var min = now.getMinutes();
+setInterval(() => {
+  timeChanged();
+}, 1);
 
-var hour = 8;
-var min = 0;
-
-min = (hour - 8) * 60 + min;
-
-if (min < 0) {
-  document.querySelector(".pointer").style.top = String(0 + 10 - 2.5) + "vh";
-} else if (min > 445) {
-  document.querySelector(".pointer").style.top = String(445 + 10 - 2.5) + "vh";
-} else {
-  document.querySelector(".pointer").style.top = String(min + 10 - 2.5) + "vh";
+function timeChanged() {
+  if (tmin != new Date().getMinutes()) {
+    pospointer();
+    tmin = new Date().getMinutes();
+  }
 }
 
-var px = document.body.clientWidth / 2;
+function pospointer() {
+  var now = new Date();
 
-var vh = document.documentElement.clientHeight / 100 * 70;
+  // var hour = now.getHours();
+  // var min = now.getMinutes();
 
-document.querySelector(".pointer").style.left = String(px + vh) + "px";
+  var hour = 8;
+  var min = 0;
+
+  min = (hour - 8) * 60 + min;
+
+  if (min < 0) {
+    document.querySelector(".pointer").style.top = String(0 + 10 - 2.5) + "vh";
+  } else if (min > 445) {
+    document.querySelector(".pointer").style.top =
+      String(445 + 10 - 2.5) + "vh";
+  } else {
+    document.querySelector(".pointer").style.top =
+      String(min + 10 - 2.5) + "vh";
+  }
+
+  var px = document.body.clientWidth / 2;
+
+  var vh = (document.documentElement.clientHeight / 100) * 70;
+
+  document.querySelector(".pointer").style.left = "10" + "px";
+}
