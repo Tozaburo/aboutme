cc73fc9b2f935d9f51217e21db644658ac94f435
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-04-21 16:38:25 +0900

    Update activity.js

--- activity.js
+++ activity.js
@@ -1,9 +1,9 @@
-function replaceDes(result){
-  document.querySelector('.description').innerHTML = result;
+function replaceDes(result) {
+  document.querySelector(".description").innerHTML = result;
 }
 
-function replaceWeek(result){
-  document.querySelector('.week').innerHTML = result;
+function replaceWeek(result) {
+  document.querySelector(".week").innerHTML = result;
 }
 
 var lang = window.navigator.language;
@@ -24,7 +24,8 @@ function clang() {
         <a href="https://www.youtube.com/@Tozaburo3D" target="_blank" rel="noopener noreferrer" class="button" onmouseover='replaceDes("Mainly uploading 3D/2D works."); replaceWeek("0.5 / week");'>Youtube</a>
         <a href="https://note.com/tozaburo" target="_blank" rel="noopener noreferrer" class="button" onmouseover='replaceDes("Posting my daily questions, what I have done, etc."); replaceWeek("0.5 / week");'>note</a>
         <a href="https://scratch.mit.edu/users/tozaburo/" target="_blank" rel="noopener noreferrer" class="button" onmouseover='replaceDes("Publishing Simulator and mathematics projects."); replaceWeek("0.5 / week");'>Scratch</a>
-        <a href="https://store.line.me/search/en?q=tozaburo" target="_blank" rel="noopener noreferrer" class="button" onmouseover='replaceDes("Stickers and themes are available."); replaceWeek("0.05 / week");'>LINE</a>
+        <a href="https://github.com/Tozaburo" target="_blank" rel="noopener noreferrer" class="button" onmouseover='replaceDes("Website and Blender add-ons. (This website too)"); replaceWeek("0.5 / week");'>GitHub</a>
+        <a href="https://store.line.me/search/en?q=tozaburo" target="_blank" rel="noopener noreferrer" class="button" onmouseover='replaceDes("Stickers and themes for LINE."); replaceWeek("0.05 / week");'>LINE</a>
         <a href="https://suzuri.jp/Tozaburo" target="_blank" rel="noopener noreferrer" class="button" onmouseover='replaceDes("T-shirts, eco-bags, and other goods."); replaceWeek("0.005 / week");'>SUZURI</a>
         <a href="https://www.curseforge.com/members/tozaburo512/projects" target="_blank" rel="noopener noreferrer" class="button" onmouseover='replaceDes("Mods for Minecraft."); replaceWeek("0.001 / week");'>CurseForge</a>
       </div>
