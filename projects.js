e52b6e6d2523b21287af671d0ae028c2f40bdd4c
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-05-04 21:00:44 +0900

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,29 +1,80 @@
-var lang = window.navigator.language;
+h1 {
+  text-align: center !important;
+  font-size: 10vmin;
+  font-weight: 700;
+  font-family: "Noto Sans JP";
+  color: #fff;
+}
 
-window.addEventListener("DOMContentLoaded", function () {
-  clang();
-});
+body {
+  margin: 0;
+  padding: 0;
+}
 
-function clang() {
-  if (lang != "ja") {
-    document.querySelector("#title").innerHTML = "Projects"
-    document.querySelector(".main").innerHTML = `
-      <div class="text">
-      <div class="title">
-        <h1>Projects</h1>
-      </div>
-      <div class="button-list">
-        <a href="https://tozaburo.github.io/aboutme/clock" class="button">New Kind of Clock</a>
-        <a href="https://tozaburo.github.io/aboutme/wine" class="button">Quality of Wine</a>
-        <a href="https://tozaburo.github.io/aboutme/file" class="button">Files to Python/JS Lists</a>
-        <p class="comingsoon">Comming soon...</p>
-      </div>
-    </div>
-    <div class="icon">
-      <a href="https://tozaburo.github.io/aboutme/">
-        <img src="home.png" alt="">
-      </a>
-    </div>
-    `;
-  }
+.main {
+  width: 100%;
+  height: 100vmin;
+  min-height: 100vh;
+  background-color: #e45a22;
+  display: flex;
+  align-items: center;
+}
+
+.text {
+  margin: auto;
+}
+
+.button{
+  cursor: pointer;
+}
+
+.comingsoon{
+  cursor: default;
+}
+
+.button, .comingsoon {
+  font-family: "Noto Sans JP";
+  font-size: 2vmin;
+  font-weight: 600;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+  width: 60vmin;
+  padding: 1vmin;
+  margin:2vmin auto;
+  border: 0.75vmin solid;
+  border-radius: 1000px;
+  display: block;
+  transition: color, background-color 0.5s;
+  background-color: transparent;
+  border-color: #fff;
+  color: #fff;
+}
+
+.button:hover {
+  background-color: #fff;
+  border-color: #fff;
+  color: #e45a22;
+}
+
+.title {
+  margin: auto;
+}
+
+.button-list {
+  margin: auto;
+}
+
+.icon a {
+  transform: 0 0;
+  position: fixed;
+  top: 5vmin;
+  left: 5vmin;
+  font-family: "Noto Sans JP";
+  font-size: 10vmin;
+  font-weight: 600;
+  cursor: pointer;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
 }
