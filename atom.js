0261b051c1b2ad9e94b3a8dab8025ea7f0f91b67
Tozaburo <85058495+Tozaburo@users.noreply.github.com>
2023-05-15 15:00:37 +0900

    Update atom.js

--- atom.js
+++ atom.js
@@ -2,8 +2,8 @@ var canvaswidth = 800;
 symbol = ["", "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og", "*1", "*2"]
 var url = location.href;
 
-// var query = Number(url.replace("https://tozaburo.github.io/aboutme/atom?", ""));
-var query = 10;
+var query = Number(url.replace("https://tozaburo.github.io/aboutme/atom?", ""));
+// var query = 10;
 
 function draw() {
     var canvas = document.getElementById('canvas');
